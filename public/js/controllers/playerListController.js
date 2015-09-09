acb1ff97a7b35f26705273db92ab298d07932684
Derek Cormier <derek.m.cormier@gmail.com>
2015-09-09 13:06:33 -0700

    Ensure the player searched for is always first in the list

--- public/js/controllers/playerListController.js
+++ public/js/controllers/playerListController.js
@@ -1,9 +1,21 @@
 app.controller("playerListController", ['$scope', '$http', '$location', function($scope, $http, $location) {
+    var playerID = "46412387";
+
+    // Request a list of the player's peers in recent games
     $http({
         method: 'GET',
-        url: $location.path() + '/players/46412387',
+        url: $location.path() + '/players/' + playerID
     }).success(function(response) {
-        console.log(response);
         $scope.players = response.players;
+
+        // Ensure the original player searched for is first in the list
+        for (var i = 0; i < $scope.players.length; i++) {
+            if ($scope.players[i].id === playerID) {
+                var temp = $scope.players[0];
+                $scope.players[0] = $scope.players[i];
+                $scope.players[i] = temp;
+                break;
+            }
+        }
     });
 }]);
