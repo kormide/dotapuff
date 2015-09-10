7d41603c5c2c95987a466bfcf15b9b96aa844216
Derek Cormier <derek.m.cormier@gmail.com>
2015-09-09 18:24:52 -0700

    Move chosen users into the comparison view

--- public/js/controllers/playerListController.js
+++ public/js/controllers/playerListController.js
@@ -1,4 +1,4 @@
-app.controller("playerListController", ['$scope', '$http', '$location', function($scope, $http, $location) {
+app.controller("playerListController", ['$scope', '$http', '$location', '$filter', function($scope, $http, $location, $filter) {
     var playerID = "46412387";
 
     // Request a list of the player's peers in recent games
@@ -6,16 +6,43 @@ app.controller("playerListController", ['$scope', '$http', '$location', function
         method: 'GET',
         url: $location.path() + '/players/' + playerID
     }).success(function(response) {
-        $scope.players = response.players;
+        if (response.success) {
+          $scope.players = response.players;
 
-        // Ensure the original player searched for is first in the list
-        for (var i = 0; i < $scope.players.length; i++) {
-            if ($scope.players[i].id === playerID) {
-                var temp = $scope.players[0];
-                $scope.players[0] = $scope.players[i];
-                $scope.players[i] = temp;
-                break;
-            }
+          // Ensure the original player searched for is first in the list
+          for (var i = 0; i < $scope.players.length; i++) {
+              if ($scope.players[i].id === playerID) {
+                  var temp = $scope.players[0];
+                  $scope.players[0] = $scope.players[i];
+                  $scope.players[i] = temp;
+                  break;
+              }
+          }
+
+        } else {
+          console.log("error: could not retrieve peers from server [" + response.reason + "]");
         }
     });
+
+    // Initialize the left and right comparison players to a "no selection" placeholder
+    $scope.playerLeft = {id: "000", name: "No Player", avatar: "http://localhost:3000/img/no-player.png"};
+    $scope.playerRight = {id: "000", name: "No Player", avatar: "http://localhost:3000/img/no-player.png"};
+
+    $scope.changeLeftPlayer = function(id) {
+      for (var i = 0; i < $scope.players.length; i++) {
+        if ($scope.players[i].id === id) {
+          $scope.playerLeft = $scope.players[i];
+          break;
+        }
+      }
+    };
+
+    $scope.changeRightPlayer = function(id) {
+      for (var i = 0; i < $scope.players.length; i++) {
+        if ($scope.players[i].id === id) {
+          $scope.playerRight = $scope.players[i];
+          break;
+        }
+      }
+    };
 }]);
